73ae35caeff051d3dfbeb6ef03d4bd7bfc6ab4ca
NMShihab <nmshihabislam@gmail.com>
2022-03-01 22:46:53 +0600

    completed

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import data from "./assets/test_data.json";
 
 export default function App() {
+  const renderList = ({ item }) => {
+    <Text style={styles.item}>{item.first_name}</Text>;
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>List Of Data</Text>
+      <FlatList
+        // horizontal
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) =>
+          item.gender == "Male" ? (
+            <Text style={styles.itemM}>
+              {item.first_name} - Gender {item.gender}
+            </Text>
+          ) : (
+            <Text style={styles.itemF}>
+              {item.first_name} - Gender {item.gender}
+            </Text>
+          )
+        }
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingLeft: 24,
+    paddingTop: 24,
+  },
+  itemM: {
+    backgroundColor: "rgb(12, 12, 128)",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    color: "#ffffff",
+  },
+  itemF: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+
+  text: {
+    fontSize: 20,
+    paddingBottom: 24,
+    paddingLeft: 15,
+    paddingTop: 20,
   },
 });
